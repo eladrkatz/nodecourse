c2dd9c1d763649036ab2b664e2ab22ddadd3179e
Elad Katz <eladk@sela.co.il>
2017-08-16 19:28:35 +0300

    stuff

--- node.server.js
+++ node.server.js
@@ -1,9 +1,22 @@
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World!');
+// }).listen(8080);
+
+
 var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    
+    setTimeout(function() {
+        res.end('Hello World!');    
+    }, 3000);
+    
 }).listen(8080);
+
 
 
 // var http = require('http');
